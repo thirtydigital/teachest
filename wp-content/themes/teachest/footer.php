6de83463fb66d8986f40dab7eb0c653080b8405a
Andy Girvan <andy.girvan@argos.co.uk>
2016-03-09 15:10:33 +0000

    Latest tweaks, removal of unused packages etc...

--- wp-content/themes/teachest/footer.php
+++ wp-content/themes/teachest/footer.php
@@ -7,6 +7,12 @@
  * @since Tea Chest 1.0
  */
 ?>
+  <div class="container">
+    <footer>
+      <p class="pull-right">Follow us:</p>
+      <p>&copy; TeaChest <?php echo date("Y"); ?></p>
+    </footer>
+  </div>
   <?php wp_footer(); ?>
 	</body>
 </html>
--- wp-content/themes/teachest/functions.php
+++ wp-content/themes/teachest/functions.php
@@ -2,6 +2,6 @@
 /*===========================
   Add WooCommerce Theme Support
 ===========================*/
-// add_theme_support( 'woocommerce' );
+add_theme_support( 'woocommerce' );
 add_filter('show_admin_bar', '__return_false');
 ?>
