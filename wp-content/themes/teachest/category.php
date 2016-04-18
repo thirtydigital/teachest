38983e2fa1b6e2465497aa1f7323965f179e9b9e
Andy Girvan <andy.girvan@argos.co.uk>
2016-04-18 15:42:45 +0100

    Categories page updated to match the same style as the Archive pages

--- wp-content/themes/teachest/category.php
+++ wp-content/themes/teachest/category.php
@@ -12,7 +12,7 @@ get_header(); ?>
   <div class="container text-center">
     <div class="col-sm-1"></div>
     <div class="col-sm-10">
-      <h1 class="display-3 text-uppercase">Our Range: <?php echo single_cat_title(); ?></h1>
+      <h1 class="display-3 text-uppercase"><?php echo single_cat_title(); ?></h1>
       <nav class="tea-categories">
         <?php wp_nav_menu( array( 'theme_location' => 'our-range-categories', 'container' => false, 'menu_class' => 'list-inline') ); ?>
       </nav>
@@ -34,17 +34,18 @@ get_header(); ?>
       $loop = new WP_Query( $args ); $loop_count = 0;
       ?>
       <?php if ( $loop->have_posts() ) : ?><?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-      <div class="col-sm-4 our-range-tea">
-        <div class="media">
-          <div class="media-left">
-            <?php the_post_thumbnail( array(100,100) ); ?>
-          </div>
-          <div class="media-body">
-            <h3 class="media-heading"><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></h3>
-            <p><?php echo get_the_excerpt(); ?></p>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 our-range-tea spacer">
+          <div class="media">
+            <div class="media-left">
+              <?php the_post_thumbnail( array(200,200) ); ?>
+              <a href="<?php the_permalink(); ?>" class="btn btn-tc-default text-uppercase spacer btn-block">View</a>
+            </div>
+            <div class="media-body">
+              <h3 class="media-heading"><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></h3>
+              <p><?php echo get_the_excerpt(); ?></p>
+            </div>
           </div>
         </div>
-      </div>
     <?php endwhile; else : ?>
       <h2>Oops... There are no teas in this category</h2>
     <?php endif; ?>
